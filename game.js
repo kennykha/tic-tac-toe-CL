cde78c8eddf2f4b464ed1121bb1f2c73ed747e15
Kenny Kha <kennykha@gmail.com>
2021-01-12 14:49:38 -0800

    Basic working mvp for tic tac toe. Missing move validation

--- game.js
+++ game.js
@@ -2,16 +2,37 @@ const prompt = require('prompt')
 
 let playerX = [];
 let playerO = [];
-let winningHand = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
+
+const winCondition = (hand, player) => {
+  let winningHand = [
+    ['0','1','2'],
+    ['3','4','5'],
+    ['6','7','8'],
+    ['0','3','6'],
+    ['1','4','7'],
+    ['2','5','8'],
+    ['0','4','8'],
+    ['2','4','6']
+  ]
+
+  let count = 0;
+  for (var i = 0; i < winningHand.length; i++) {
+    // console.log(count)
+    if (count === 3) {
+      console.log(player + 'is the winner!')
+      break;
+    }
+    for (var j = 0; j < winningHand[i].length; j++) {
+      // console.log('Winning Hand', winningHand[i][j])
+      // console.log(hand.includes(winningHand[i][j]))
+      if (hand.includes(winningHand[i][j])) {
+        count++
+      } else {
+        count = 0;
+      }
+    }
+  }
+}
 
 let board = [
   '0',
@@ -54,10 +75,12 @@ let playGame = (player) => {
     // }
     if (player === 'X') {
       playerX.push(result.position)
+      winCondition(playerX, player)
       console.log('Player X Hand: ', playerX)
       playGame('O')
     } else {
       playerO.push(result.position)
+      winCondition(playerO, player)
       console.log('Player O Hand: ', playerO)
       playGame('X')
     }
